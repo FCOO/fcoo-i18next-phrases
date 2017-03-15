228f0f4d85eaa0d8704d1a5119f0ca7c92f650d5
Niels Holt <niels@steenbuchholt.dk>
2017-03-15 12:01:37 +0100

    Bug fix in gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -17,14 +17,11 @@ module.exports = function(grunt) {
                 "haveStyleSheet": false, //true if the packages have css and/or scss-files
                 "haveGhPages"   : true,  //true if there is a branch "gh-pages" used for demos
 
-                //Cmd to be run at the start of prod-task. Multi cmd can be seperated by "&": copy json-files from src to dist
-                "beforeProdCmd": "grunt copy:prod --gruntfile gruntfile_copy.js", 
+                "beforeProdCmd": "",  //Cmd to be run at the start of prod-task. Multi cmd can be seperated by "&"
+                "beforeDevCmd" : "",  //Cmd to be run at the start of dev-task. Multi cmd can be seperated by "&"
 
-                //Cmd to be run at the start of dev-task. Multi cmd can be seperated by "&": copy copy json-files from src to demo
-                "beforeDevCmd" : "grunt copy:dev --gruntfile gruntfile_copy.js", 
-
-                "afterProdCmd" : "",     //Cmd to be run at the end of prod-task
-                "afterDevCmd"  : "",     //Cmd to be run at the end of dev-task
+                "afterProdCmd" : "grunt copy:prod --gruntfile gruntfile_copy.js", //Cmd to be run at the end of prod-task: copy json-files from src to dist
+                "afterDevCmd"  : "grunt copy:dev --gruntfile gruntfile_copy.js",  //Cmd to be run at the end of dev-task: copy copy json-files from src to demo
 
                 "DEBUG"        : false   //if true different debugging is on and the tempoary files are not deleted
             }
